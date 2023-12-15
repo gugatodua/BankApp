b922e31f266b157338f2716f9bf51f1d83172a2d
kakhatodua2002@gmail.com <kakhatodua2002@gmail.com>
2023-12-15 20:32:13 +0400

    AccountAndDepositAdder

---
+++ BankApp/AccountAndDepositAdder.cs
@@ -0,0 +1,40 @@
+﻿using BankApp.Domain;
+using BankApp.Models;
+
+namespace BankApp.Api
+{
+    public class AccountAndDepositAdder : IAccountAndDepositAdder
+    {
+        private readonly BankDbContext _dbContext;
+        public AccountAndDepositAdder(BankDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddAccountRecord(
+           string userId,
+           string iban,
+           string currency,
+           bool isDeposit = false,
+           DateTime? withdrawDate = null)
+        {
+            var user = _dbContext.Users
+             .Where(x => x.Id == userId)
+             .FirstOrDefault();
+
+            var account = new Account
+            {
+                Id = Guid.NewGuid(),
+                Balance = 0,
+                Iban = iban,
+                User = user,
+                Currency = currency,
+                IsDeposit = isDeposit,
+                WithdrawDate = withdrawDate
+            };
+
+            _dbContext.Add(account);
+            _dbContext.SaveChanges();
+        }
+    }
+}
